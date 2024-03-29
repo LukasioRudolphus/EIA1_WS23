d02cc5bb8216d5cee7c49b7029ad102fb799930a
LukasioRudolphus <139594605+LukasioRudolphus@users.noreply.github.com>
2024-03-29 12:41:08 +0100

    Stuff

---
+++ VSCode/Stuff/test.js
@@ -0,0 +1,6 @@
+document.querySelector(".change").addEventListener("click", farbe);
+
+ function farbe(){
+    const leucht = document.querySelector(".eins");
+    leucht.style.backgroundColor = "red";
+ }
